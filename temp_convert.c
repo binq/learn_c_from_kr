3f0fa794c2b5d42e56fc2245b869b222b5d57fa8
Vanson Samuel <vanson@Outland.local>
2009-12-30 10:39:44 -0500

    Finished temperature converter.

---
+++ temp_convert.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main() {
+	float start_num = 0;
+	float end_num = 300;
+	float step = 20;
+	float source_temp, converted_temp;
+
+	for(source_temp=start_num; source_temp <= end_num; source_temp += step) {
+		converted_temp = (5.0/9.0)*(source_temp-32.0);
+		printf("%7.2f %7.2f\n", source_temp, converted_temp);
+	}
+}
