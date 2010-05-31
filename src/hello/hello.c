8cd229a06009924bcdbe89932b72a8d6e69cb138
Vanson Samuel <vs@binq.com>
2010-05-31 02:12:49 -0400

    Hello world take argv

--- src/hello/hello.c
+++ src/hello/hello.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <stdio.h>
 
-main() {
-	printf("Hello World\n");	
+main(int argc, char **argv) {
+	char *name = argc > 1 ? argv[1] : "World";
+	printf("Hello %s\n", name);
 }
