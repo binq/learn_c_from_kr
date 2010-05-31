8f5e2527aaf42590bf81104e4499cdfdd2aabf5f
Vanson Samuel <vs@binq.com>
2010-05-31 02:49:56 -0400

    using macros

--- src/temp_convert/temp_convert.c
+++ src/temp_convert/temp_convert.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
+#define START_NUM 0
+#define END_NUM 300
+#define STEP 20
 
 main() {
-	int start_num = 0;
-	int end_num = 300;
-	int step = 20;
 	int source_temp;
 	float converted_temp;
 
 	printf("Fahrenheit => Celsius\n", source_temp, converted_temp);
-	for(source_temp=end_num; source_temp >= start_num; source_temp -= step) {
+	for(source_temp=END_NUM; source_temp >= START_NUM; source_temp -= STEP) {
 		converted_temp = (5.0/9.0)*(source_temp-32.0);
 		printf("%10u => %7.2f\n", source_temp, converted_temp);
 	}
