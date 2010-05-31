b4bdcd39a5505d19614eba33201a8849d7a5182b
Vanson Samuel <vs@binq.com>
2010-05-31 02:42:15 -0400

    added table header and made inputs into ints

--- src/temp_convert/temp_convert.c
+++ src/temp_convert/temp_convert.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 main() {
-	float start_num = 0;
-	float end_num = 300;
-	float step = 20;
-	float source_temp, converted_temp;
+	int start_num = 0;
+	int end_num = 300;
+	int step = 20;
+	int source_temp;
+	float converted_temp;
 
+	printf("Fahrenheit => Celsius\n", source_temp, converted_temp);
 	for(source_temp=start_num; source_temp <= end_num; source_temp += step) {
 		converted_temp = (5.0/9.0)*(source_temp-32.0);
-		printf("%7.2f Fahrenheit => %7.2f Celsius\n", source_temp, converted_temp);
+		printf("%10u => %7.2f\n", source_temp, converted_temp);
 	}
 }
